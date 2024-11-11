a3c5d9898352fcbbbd27ae6deba91ebf27af76ee
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-11-11 10:48:32 +0500

    updated api code

--- lib/utils/pexel_api.dart
+++ lib/utils/pexel_api.dart
@@ -2,13 +2,39 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PexelsAPI {
+  final String _baseUrl = 'https://api.pexels.com/v1';
   final String _apiKey =
       '6LB9dno1G5s5ZBUtjOhvp6OdvdUkZYuYmCMBdPO8g4vnnvtT8a7kTpvt'; // Replace with your actual Pexels API key
+
+  // Fetches a single image based on a search term (e.g., "Nature" or "Space")
+  Future<String?> fetchSingleImage(String searchTerm) async {
+    final url = Uri.parse('$_baseUrl/search?query=$searchTerm&per_page=1');
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': _apiKey,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['photos'] != null && data['photos'].isNotEmpty) {
+          return data['photos'][0]['src']['large'];
+        }
+      } else {
+        print('Failed to load image for $searchTerm: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching image for $searchTerm: $e');
+    }
+    return null;
+  }
 
   // Function to fetch wallpapers based on a search query
   Future<List<String>> fetchWallpapers(String query) async {
     final url =
-        Uri.parse('https://api.pexels.com/v1/search?query=$query&per_page=20');
+        Uri.parse('$_baseUrl/search?query=$query&per_page=20');
 
     final response = await http.get(url, headers: {
       'Authorization': _apiKey,
